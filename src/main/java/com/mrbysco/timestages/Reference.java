f14b4abc9a686a190a7de500f43b9f91fe52de03
sporotofpoorety <pereira.m.biel@gmail.com>
2025-10-14 21:12:51 -0300

    Deleted all CraftTweaker dependency and support

--- src/main/java/com/mrbysco/timestages/Reference.java
+++ src/main/java/com/mrbysco/timestages/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "timestages";
 	public static final String MOD_NAME = "Time Stages";
 	public static final String VERSION = "1.4.1";
-	public static final String DEPENDENCIES = "required-after:crafttweaker;required-after:bookshelf@[2.3.544,);required-after:gamestages@[2.0.87,);";
+	public static final String DEPENDENCIES = "required-after:bookshelf@[2.3.544,);required-after:gamestages@[2.0.87,);";
 }
